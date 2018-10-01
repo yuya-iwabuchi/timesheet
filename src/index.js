3f15752dd0997b94fe4ad4f6e559fc6d3cc87203
Yuya <yuya.iwabuchi@gmail.com>
2018-09-30 21:37:25 -0400

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+unregister();
+
